7b01ccc821c5822522a4c85ad92c4b7acda90210
bolan2014 <1139217488@qq.com>
2015-11-22 20:43:43 +0800

    first commit

---
+++ easy/SubmissionDetails.py
@@ -0,0 +1,11 @@
+'''
+method: difference of length between set and list
+'''
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(nums) != len(set(nums))
